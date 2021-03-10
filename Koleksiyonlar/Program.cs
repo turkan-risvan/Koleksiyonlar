3a4e915a5f1dec622561aae992a1759f2d51ca87
ACER <ACER@DESKTOP-TRKNRSV>
2021-03-10 13:14:44 +0300

    Add project files.

---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] isimler = new string[] { "Türkan", "Sevde", "Kübra", "Birsen", "Delal" };
+            //Console.WriteLine(isimler[0]);
+            //Console.WriteLine(isimler[1]);
+            //Console.WriteLine(isimler[2]);
+            //Console.WriteLine(isimler[3]);
+            //Console.WriteLine(isimler[4]);
+
+            //isimler = new string[6];
+            //isimler[5] = "Fatma";
+            //Console.WriteLine(isimler[5]);
+            //Console.WriteLine(isimler[0]);
+
+
+            List<string> isimler2 = new List<string> { "Türkan", "Sevde", "Kübra", "Birsen", "Delal" };
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[3]);
+            Console.WriteLine(isimler2[4]);
+
+            isimler2.Add("Ayşe");
+            Console.WriteLine(isimler2[5]);
+            Console.WriteLine(isimler2[0]);
+        }
+    }
+}
